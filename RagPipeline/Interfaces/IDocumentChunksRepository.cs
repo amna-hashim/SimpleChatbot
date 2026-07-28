ee3602e6e8f498ed3119c4a954c2d360150b6083
ahashim123 <amnahashimy2k11@gmail.com>
2026-07-28 10:37:17 -0400

    deleted

--- RagPipeline/Interfaces/IDocumentChunksRepository.cs
+++
@@ -1,9 +0,0 @@
-using RagPipeline.Models;
-
-namespace RagPipeline.Interfaces;
-
-public interface IDocumentChunksRepository
-{
-    Task AddWithEmbeddingAsync(RagDocumentChunk chunk, float[] embedding, CancellationToken ct);
-    Task<List<RagDocumentChunk>> GetBySourceDocumentIdAsync(int sourceDocumentId, CancellationToken ct);
-}
